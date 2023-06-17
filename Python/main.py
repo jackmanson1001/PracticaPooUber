ac1a5255408c7f54a393d952eaf8543f99d3b6cd
jackmanson1001 <jackmanson1001@gmail.com>
2023-06-17 12:03:08 -0500

    Python POO con Clases

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+# importando la clase
+from car import Car
+
+if __name__ == "__main__":
+    print ("Hola mundo")
+    car = Car()
+    car.license = "JLI245"
+    car.driver = "Julian Polo"
+    print(vars(car))
+
+    # cachar2 = Car()
+    # cachar2.license = "HOH421"
+    # print(vars(cachar2))
+
+
+
