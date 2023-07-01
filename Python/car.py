388df9d9b92ec2bd606b34a216fc9227bb3f6bd5
jackmanson1001 <jackmanson1001@gmail.com>
2023-07-01 16:15:11 -0500

    agregando clases Payments Herencia con Python

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,11 @@
-class Car:
+from account import Account
+
+class Car(Account):
     id          = int 
     license     = str
-    driver      = str   
+    driver      = Account  
     passenger   = str
+
+def __init__(self,license,driver):
+    super.__init__(driver)
+    self.license = license
--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,19 @@
 # importando la clase
+from account import Account
 from car import Car
 
 if __name__ == "__main__":
     print ("Hola mundo")
-    car = Car()
-    car.license = "JLI245"
-    car.driver = "Julian Polo"
-    print(vars(car))
+    
+    #car = Car() #Instancia la clase Car
+    #car.license = "JLI245" #Envia datos a variable nativa License
+    #car.driver = "Julian Polo" #Envia datos a variable nativa Heredada driver
+    #print(vars(car))
 
-    # cachar2 = Car()
-    # cachar2.license = "HOH421"
-    # print(vars(cachar2))
+    car2 = Car("HDL235",Account("Jack","4214352"))
+    print(vars(car2))
+    print(vars(car2.driver))
+
 
 
 
